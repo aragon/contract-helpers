c0ac7a6bdeb37419a1a1362d623c84be8ea87a80
Gabriel <gabrielpk.18@gmail.com>
2020-04-13 17:34:25 -0300

    Remove reverString and move makeErrorMapping to utils

--- packages/test-helpers/utils.js
+++ packages/test-helpers/utils.js
@@ -9,7 +9,23 @@ const onlyIf = condition => {
 
 const isValidTxHash = txHash => /^0x([A-Fa-f0-9]{64})$/.test(txHash)
 
+function makeErrorMappingProxy (target) {
+  return new Proxy(target, {
+    get (target, property) {
+      if (property in target) {
+        return target[property]
+      }
+
+      throw new Error(`Could not find error ${property} in error mapping`)
+    },
+    set () {
+      throw new Error('Unexpected set to error mapping')
+    }
+  })
+}
+
 module.exports = {
   onlyIf,
-  isValidTxHash
+  isValidTxHash,
+  makeErrorMappingProxy
 }
