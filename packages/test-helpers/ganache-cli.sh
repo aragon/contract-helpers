6446b4e6c14d666513a97f8d52ec9b20a9ddad13
Gabriel <gabrielpk.18@gmail.com>
2020-04-14 01:04:48 -0300

    Fix: move to buidler compatible ganache-cli script

--- packages/test-helpers/ganache-cli.sh
+++ packages/test-helpers/ganache-cli.sh
@@ -7,26 +7,16 @@ set -o errexit
 trap cleanup EXIT
 
 cleanup() {
-  # Kill the RPC instance that we started (if we started one and if it's still running).
+  # Kill the ganache instance that we started (if we started one and if it's still running).
   if [ -n "$rpc_pid" ] && ps -p $rpc_pid > /dev/null; then
     kill -9 $rpc_pid
   fi
 }
 
-setup_coverage_variables() {
-  PORT=${PORT-8555}
-  BALANCE=${BALANCE-100000}
-  GAS_LIMIT=${GAS_LIMIT-0xfffffffffff}
-  NETWORK_ID=${NETWORK_ID-16}
-  ACCOUNTS=${ACCOUNTS-200}
-}
+ganache_port=8545
 
-setup_testing_variables() {
-  PORT=${PORT-8545}
-  BALANCE=${BALANCE-100000}
-  GAS_LIMIT=${GAS_LIMIT-8000000}
-  NETWORK_ID=${NETWORK_ID-15}
-  ACCOUNTS=${ACCOUNTS-200}
+rpc_running() {
+  nc -z localhost "$ganache_port"
 }
 
 rpc_running() {
@@ -34,25 +24,35 @@ rpc_running() {
 }
 
 start_ganache() {
-  echo "Starting ganache-cli..."
-  node_modules/.bin/ganache-cli -i ${NETWORK_ID} -l ${GAS_LIMIT} -a ${ACCOUNTS} -e ${BALANCE} -p ${PORT} > /dev/null &
-  rpc_pid=$!
-  echo "Waiting for ganache to launch on port "$PORT"..."
+  if [ "$SOLIDITY_COVERAGE" = true ]; then
+    export RUNNING_COVERAGE=true
+  else
+    echo "Starting our own ganache instance"
 
-  while ! rpc_running; do
-    sleep 0.1 # wait for 1/10 of the second before check again
-  done
+    node_modules/.bin/ganache-cli -l 8000000 --port "$ganache_port" -m "explain tackle mirror kit van hammer degree position ginger unfair soup bonus" > /dev/null &
 
-  echo "Running ganache-cli with pid ${rpc_pid} in port ${PORT}"
+    rpc_pid=$!
+
+    echo "Waiting for ganache to launch on port "$ganache_port"..."
+
+    while ! rpc_running; do
+      sleep 0.1 # wait for 1/10 of the second before check again
+    done
+
+    echo "Ganache launched!"
+  fi
 }
 
+if rpc_running; then
+  echo "Using existing ganache instance"
+else
+  start_ganache
+fi
+
+echo "Buidler version $(npx buidler --version)"
 
 if [ "$SOLIDITY_COVERAGE" = true ]; then
-  setup_coverage_variables
-  start_ganache
   node_modules/.bin/buidler coverage --network coverage "$@"
 else
-  setup_testing_variables
-  start_ganache
   node_modules/.bin/buidler test "$@"
 fi
