b3682bc7300a6efb8f4ee17d9ca6c0e2ec0abc0b
Brett Sun <qisheng.brett.sun@gmail.com>
2020-07-14 20:33:29 +0200

    test-helpers: re-organize utility files

--- packages/test-helpers/block.js
+++
@@ -1,8 +0,0 @@
-module.exports = (web3) => (n) => {
-  return new Promise((resolve, reject) => {
-    web3.eth.getBlock(n, (err, res) => {
-      if (err || !res) return reject(err)
-      resolve(res)
-    })
-  })
-}
