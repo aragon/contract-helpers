e137c1870cec5c0822ff6b3fd7e7be46bf9d1b08
ßingen <bingen@aragon.one>
2020-07-16 18:32:29 +0200

    test-helpers: Flesh out empty bytes, add comment

--- packages/test-helpers/src/bytes.js
+++ packages/test-helpers/src/bytes.js
@@ -1,4 +1,4 @@
-const EMPTY_BYTES = '0x00'
+const EMPTY_BYTES = '0x00' // Empty bytes from Solidity getters are returned with 2 zeroes
 const ZERO_BYTES32 = '0x' + '0'.repeat(64) // 0x0000...0000
 
 function stripBytePrefix(bytes) {
