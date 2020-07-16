e1f15949ea6f2790a6f664cf0e4e3d9aba6ecb46
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-07-16 16:55:03 -0300

    test: fix node version helper

--- packages/test-helpers/src/asserts/assertThrow.js
+++ packages/test-helpers/src/asserts/assertThrow.js
@@ -16,7 +16,7 @@ async function assertThrows(
       ? await blockOrPromise()
       : await blockOrPromise
   } catch (error) {
-    if (isGeth(ctx)) {
+    if (await isGeth(ctx)) {
       // With geth, we are only provided the transaction receipt and have to decode the failure
       // ourselves.
       const status = error.receipt.status
