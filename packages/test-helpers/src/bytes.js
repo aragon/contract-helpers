9542901e4f6df039e3fdbbe9b4135283bae6776e
ßingen <bingen@aragon.one>
2020-07-16 16:10:06 +0200

    test-helpers: Use repeat for long constants, add comments

--- packages/test-helpers/src/bytes.js
+++ packages/test-helpers/src/bytes.js
@@ -1,5 +1,5 @@
 const EMPTY_BYTES = '0x'
-const ZERO_BYTES32 = '0x' + '0'.repeat(64)
+const ZERO_BYTES32 = '0x' + '0'.repeat(64) // 0x0000...0000
 
 function stripBytePrefix(bytes) {
   return bytes.substring(0, 2) === '0x' ? bytes.slice(2) : bytes
